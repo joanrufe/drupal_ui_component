74fc5521aa5f36fc2a191c27e03d459ef7736760
Antonio Ruiz <stayforawhile@gmail.com>
2018-02-28 13:40:47 +0100

    Using webpack relative path
    api now using location.origin as url

--- js-src/datepicker/api.js
+++ js-src/datepicker/api.js
@@ -1,6 +1,4 @@
-import {findIndex} from 'lodash'
-
-const baseUrl = 'http://127.0.0.1:8088' // @TODO: process.env.HOST ?
+const baseUrl = location.origin || 'http://drupalvm.test' // @TODO: process.env.HOST
 
 export default {
     fetchUsers: () => fetch(`${baseUrl}/api/users_with_photo`)
